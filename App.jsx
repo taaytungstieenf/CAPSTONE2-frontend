639160af696aed0a67edeea03d0ec4927d4b8e43
Nguyễn Đức Tây <129637919+taaytungstieenf@users.noreply.github.com>
2024-10-28 19:36:05 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+//import './App.css' (this is a css file)
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ListCustomerComponent from './components/ListCustomerComponent'
+
+function App() {
+
+  return (
+    <>
+      <ListCustomerComponent/>
+    </>
+  )
+}
+
+export default App
